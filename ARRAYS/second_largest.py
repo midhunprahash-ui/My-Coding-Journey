df4289bc0a3c7d725db1d19b3671d970c650d58f
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-09 10:58:18 +0530

    feat: add function to find the second largest number in an array

---
+++ ARRAYS/second_largest.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        largest=-1
+        second_largest=-1
+        
+        for i in range(len(arr)):
+            if arr[i]>largest:
+                largest=arr[i]
+        for i in range(len(arr)):
+            if arr[i]>second_largest and arr[i]!= largest:
+                second_largest=arr[i]
+        return second_largest
